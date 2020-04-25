aeb5feeaa19ea997f9755209fe215a33caf8e297
Carlos <carlos4561059919@gmail.com>
2020-04-25 12:25:13 -0500

    version 1

---
+++ ejemplo.js
@@ -0,0 +1,19 @@
+
+
+async function obtenerClientes() {
+    const clientes= new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve(`Clientes Descargados...`);
+        }, 2000);
+    });
+    const error= false;
+    if(!error){
+        const respuesta= await clientes;
+        return respuesta;
+    }else{
+        await Promise.reject(`Hubo un error...`);
+    }
+}
+obtenerClientes()
+    .then(res =>console.log(res))
+    .catch(error => console.log(error));
